e461c4a70ae7e87092397cc6b00eae98fe8657d1
Indy Zhu <indyz@ntdev.microsoft.com>
2018-07-25 17:31:06 -0700

    Move Meta Command definition into common header file

--- compute-only-sample/costestmc/costestmc.cpp
+++ compute-only-sample/costestmc/costestmc.cpp
@@ -15,6 +15,7 @@
 #include <memory>
 #include <list>
 
+#include "CosMetaCommand.h"
 
 template<class T> class D3DPointer
 {
@@ -361,7 +362,6 @@ void D3DBuffer::CopyTo(ID3D12GraphicsCommandList * inCommandList, D3DOutputBuffe
 }
 
 
-
 struct BufType
 {
     int i;
@@ -370,23 +370,6 @@ struct BufType
 
 #define NUM_ELEMENTS     1024
 #define THREADS_IN_GROUP 4      // Expressed in the shader
-
-// {980BFE94-D9FC-4AA7-837B-ACD6798383F7}
-// Identity meta command copies input resource into output resource
-const GUID GUID_IDENTITY =
-    { 0x980bfe94, 0xd9fc, 0x4aa7,{ 0x83, 0x7b, 0xac, 0xd6, 0x79, 0x83, 0x83, 0xf7 } };
-
-#pragma pack(push,4)
-struct IdentityMetaCommandCreationParameters
-{
-    UINT64 BufferSize;
-};
-struct IdentityMetaCommandExecutionParameters
-{
-    D3D12_GPU_VIRTUAL_ADDRESS Input;
-    D3D12_GPU_VIRTUAL_ADDRESS Output;
-};
-#pragma pack(pop)
 
 int main()
 {
